70b9a78c2e90730a50aeb2a2b693104c9e1abd14
Ujjval Mishra <mishraujjwal361@gmail.com>
2024-05-28 12:55:10 +0530

    Added code for new lines between numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -20,5 +20,17 @@ describe StringCalculator do
         expect(StringCalculator.add("1,5")).to eq(6)
       end
     end
+
+    context "given 1,2,3,4,5,7,89,1,90.200,200,300,34" do
+      it "returns 936" do
+        expect(StringCalculator.add("1,2,3,4,5,7,89,1,90,200,200,300,34")).to eq(936)
+      end
+    end
+
+    context "given new lines between numbers (1\n2,3)" do
+      it "returns 6" do
+        expect(StringCalculator.add("1\n2,3")).to eq(6)
+      end
+    end
   end
 end
