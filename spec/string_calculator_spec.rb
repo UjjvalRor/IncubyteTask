be5f4ab10afbed4015566e2291981dbd67aaea30
Ujjval Mishra <mishraujjwal361@gmail.com>
2024-05-28 12:50:18 +0530

    Added code for given string and its test case

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -8,5 +8,17 @@ describe StringCalculator do
         expect(StringCalculator.add("")).to eq(0)
       end
     end
+
+    context "given 1" do
+      it "returns 1" do
+        expect(StringCalculator.add("1")).to eq(1)
+      end
+    end
+
+    context "given 1,5" do
+      it "returns 6" do
+        expect(StringCalculator.add("1,5")).to eq(6)
+      end
+    end
   end
 end
