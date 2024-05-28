d4853b08f2cbf3a38db8dce338f945b9467ebad2
Ujjval Mishra <mishraujjwal361@gmail.com>
2024-05-28 13:05:59 +0530

    Added code for delimeters and its test case

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -32,5 +32,11 @@ describe StringCalculator do
         expect(StringCalculator.add("1\n2,3")).to eq(6)
       end
     end
+
+    context "given string with delimiter (//;\n1;2)" do
+      it "returns 3" do
+        expect(StringCalculator.add("//;\n1;2")).to eq(3)
+      end
+    end
   end
 end
